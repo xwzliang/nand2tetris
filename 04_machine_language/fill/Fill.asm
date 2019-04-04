880af264c3f9c77b926b9569b7f5b0feaf67c07d
broliang <18706703437@163.com>
2019-04-04 19:44:32 +0800

    Finish program Fill (when press a key on keyboard, blacken the screen) using assembly

--- 04_machine_language/fill/Fill.asm
+++ 04_machine_language/fill/Fill.asm
@@ -12,3 +12,90 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// if (KBD=0){
+	// SCREEN=0;
+// }
+// else{
+	// SCREEN=-1;
+// }
+
+	// if KBD = 0 goto WHITE
+	@KBD
+	D=M
+	@WHITE
+	D;JEQ
+	// if KBD != 0 goto BLACK
+	@BLACK
+	D;JNE
+(WHITE)
+	// i = 8k - 1
+	@8191
+	D=A
+	@i
+	M=D
+(LOOP_WHITE)
+	// set register SCREEN + i white
+	@i
+	D=M
+	@SCREEN
+	A=A+D
+	M=0
+	// if i=0, goto END_WHITE
+	@i
+	D=M
+	@END_WHITE
+	D;JEQ
+	// i--
+	@1
+	D=A
+	@i
+	M=M-D
+	// goto LOOP_WHITE
+	@LOOP_WHITE
+	0;JMP
+(END_WHITE)
+	// if KBD != 0 goto BLACK
+	@KBD
+	D=M
+	@BLACK
+	D;JNE
+	// else goto END_WHITE
+	@END_WHITE
+	0;JMP
+
+(BLACK)
+	// i = 8k - 1
+	@8191
+	D=A
+	@i
+	M=D
+(LOOP_BLACK)
+	// set register SCREEN + i black
+	@i
+	D=M
+	@SCREEN
+	A=A+D
+	M=-1
+	// if i=0, goto END_BLACK
+	@i
+	D=M
+	@END_BLACK
+	D;JEQ
+	// i--
+	@1
+	D=A
+	@i
+	M=M-D
+	// goto LOOP_BLACK
+	@LOOP_BLACK
+	0;JMP
+(END_BLACK)
+	// if KBD = 0 goto WHITE
+	@KBD
+	D=M
+	@WHITE
+	D;JEQ
+	// else goto END_BLACK
+	@END_BLACK
+	0;JMP
