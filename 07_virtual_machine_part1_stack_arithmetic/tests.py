0b00e0b39aa40cfdbb6b9142d86c1519f9ddb7ee
broliang <18706703437@163.com>
2019-07-07 20:12:34 +0800

    Refactoring: modify ./07_virtual_machine_part1_stack_arithmetic/tests.py to make general process of tests as a method run_test

--- 07_virtual_machine_part1_stack_arithmetic/tests.py
+++ 07_virtual_machine_part1_stack_arithmetic/tests.py
@@ -11,17 +11,20 @@ class VMtranslator(unittest.TestCase):
         self.given_vm_emulator_tool = self.cwd.parent / 'tools/VMEmulator.sh'
         self.my_vm_translator = self.cwd / 'VMtranslator.py'
 
-    def test_stack_arithmetic_simple_add(self):
-        simple_add_vm = self.cwd / 'test/StackArithmetic/SimpleAdd/SimpleAdd.vm'
-        simple_add_asm = simple_add_vm.with_suffix('.asm')
-        simple_add_tst = simple_add_vm.parent / 'SimpleAddVME.tst'
+    def run_test(self, vm_file):
+        asm_file = vm_file.with_suffix('.asm')
+        tst_file = vm_file.parent / (vm_file.stem + 'VME.tst')
         # Run my VMtranslator to generate asm file
-        proc_translate = sp.run([self.my_vm_translator.as_posix(), simple_add_vm.as_posix()], stdout=sp.PIPE, universal_newlines=True)
-        self.assertEqual(proc_translate.stdout, 'Successfully translate to assembly ' + simple_add_asm.as_posix() + '\n')
-        self.assertTrue(simple_add_asm.exists())
+        proc_translate = sp.run([self.my_vm_translator.as_posix(), vm_file.as_posix()], stdout=sp.PIPE, universal_newlines=True)
+        self.assertEqual(proc_translate.stdout, 'Successfully translate to assembly ' + asm_file.as_posix() + '\n')
+        self.assertTrue(asm_file.exists())
         # Use given vm emulator to compare generated asm file
-        proc_compare = sp.run([self.given_vm_emulator_tool.as_posix(), simple_add_tst], stdout=sp.PIPE, universal_newlines=True)
+        proc_compare = sp.run([self.given_vm_emulator_tool.as_posix(), tst_file], stdout=sp.PIPE, universal_newlines=True)
         self.assertEqual(proc_compare.stdout, 'End of script - Comparison ended successfully\n')
+
+    def test_stack_arithmetic_simple_add(self):
+        vm_file = self.cwd / 'test/StackArithmetic/SimpleAdd/SimpleAdd.vm'
+        self.run_test(vm_file)
 
 if __name__ == '__main__':
     unittest.main()
