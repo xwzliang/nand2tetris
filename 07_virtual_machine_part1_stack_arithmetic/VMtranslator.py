27724af76baf5961da45bbcd541189b607a83e8a
broliang <18706703437@163.com>
2019-07-07 19:56:17 +0800

    Modify ./07_virtual_machine_part1_stack_arithmetic/VMtranslator.py to support write translated asm code for directory input

--- 07_virtual_machine_part1_stack_arithmetic/VMtranslator.py
+++ 07_virtual_machine_part1_stack_arithmetic/VMtranslator.py
@@ -12,15 +12,17 @@ assert path_input.exists(), "Path not exists."
 if path_input.is_dir():	# Translate all vm files in directory
     vmfiles = [f for f in path_input.glob('*.vm')]
     assert vmfiles, "No vm file in this directory."
+    out_asm_file = path_input / (path_input.name + '.asm')	# Out asm file name same as directory name
+    returned_contents = []
     for vmfile in vmfiles:
         psr = Parser(vmfile)
-        returned_contents = psr.parse()
+        returned_contents += psr.parse()
 else:	# Translate one vm file
     vmfile = path_input
     out_asm_file = path_input.with_suffix('.asm')
     psr = Parser(vmfile)
     returned_contents = psr.parse()
-    cw = CodeWriter(returned_contents, out_asm_file)
-    cw.write()
-# print(returned_contents)
+
+cw = CodeWriter(returned_contents, out_asm_file)
+cw.write()
 print('Successfully translate to assembly ' + out_asm_file.as_posix())
