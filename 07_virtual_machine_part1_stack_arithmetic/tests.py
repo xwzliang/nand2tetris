f34449dc9cfb76fdc34acf8666c4caa503c57bb1
broliang <18706703437@163.com>
2019-07-07 22:26:33 +0800

    Fix error in ./07_virtual_machine_part1_stack_arithmetic/tests.py@ Should use given CPUEmulator instead of VMEmulator

--- 07_virtual_machine_part1_stack_arithmetic/tests.py
+++ 07_virtual_machine_part1_stack_arithmetic/tests.py
@@ -8,18 +8,18 @@ class VMtranslator(unittest.TestCase):
 
     def setUp(self):
         self.cwd = Path.cwd()
-        self.given_vm_emulator_tool = self.cwd.parent / 'tools/VMEmulator.sh'
+        self.given_cpu_emulator_tool = self.cwd.parent / 'tools/CPUEmulator.sh'
         self.my_vm_translator = self.cwd / 'VMtranslator.py'
 
     def run_test(self, vm_file):
         asm_file = vm_file.with_suffix('.asm')
-        tst_file = vm_file.parent / (vm_file.stem + 'VME.tst')
+        tst_file = vm_file.with_suffix('.tst')
         # Run my VMtranslator to generate asm file
         proc_translate = sp.run([self.my_vm_translator.as_posix(), vm_file.as_posix()], stdout=sp.PIPE, universal_newlines=True)
         self.assertEqual(proc_translate.stdout, 'Successfully translate to assembly ' + asm_file.as_posix() + '\n')
         self.assertTrue(asm_file.exists())
-        # Use given vm emulator to compare generated asm file
-        proc_compare = sp.run([self.given_vm_emulator_tool.as_posix(), tst_file], stdout=sp.PIPE, universal_newlines=True)
+        # Use given CPU emulator to compare generated asm file
+        proc_compare = sp.run([self.given_cpu_emulator_tool.as_posix(), tst_file], stdout=sp.PIPE, universal_newlines=True)
         self.assertEqual(proc_compare.stdout, 'End of script - Comparison ended successfully\n')
 
     def test_stack_arithmetic_simple_add(self):
