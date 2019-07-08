8b93b6087534a93798cfb8cf113a93ec6d12af62
broliang <18706703437@163.com>
2019-07-08 16:30:09 +0800

    Modify ./07_virtual_machine_part1_stack_arithmetic/code_writer.py: Finish translating stack arithmetic operators.

--- 07_virtual_machine_part1_stack_arithmetic/code_writer.py
+++ 07_virtual_machine_part1_stack_arithmetic/code_writer.py
@@ -4,17 +4,112 @@ class CodeWriter:
         self.out_file = out_file
         self.command_contents = command_contents
 
-    def translate_arithmetic(self, operator):
+    def translate_arithmetic(self, operator, index):
         """Generate the assembly code that is the translation of the given arithmetic command."""
         assembly_codes = []
         if operator == 'add':
             assembly_codes = [
                     	      '@SP', 
-                    	      'M=M-1',	# SP--
-                              'A=M',
+                    	      'AM=M-1',	# SP--, A=M-1
                               'D=M',	# D=Y
                               'A=A-1',
                               'M=D+M',	# X=Y+X
+                              ]
+        elif operator == 'sub':
+            assembly_codes = [
+                              '@SP', 
+                    	      'AM=M-1',	# SP--, A=M-1
+                              'D=M',	# D=Y
+                              'A=A-1',
+                              'M=M-D',	# X=X-Y
+                              ]
+        elif operator == 'neg':
+            assembly_codes = [
+                              '@SP', 
+                              'A=M-1',
+                              'M=-M',	# Y=-Y
+                              ]
+        elif operator == 'eq':
+            assembly_codes = [
+                              '@SP', 
+                    	      'AM=M-1',	# SP--, A=M-1
+                              'D=M',	# D=Y
+                              'A=A-1',
+                              'D=M-D',	# D=X-Y
+                              '@IS_EQUAL_{}'.format(index), 
+                              'D;JEQ',	# if D=0 jump to label IS_EQUAL
+                              '@SP',
+                              'A=M-1',
+                              'M=0',	# if D!=0, M=False
+                              '@END_COMPARE_EQ_{}'.format(index),
+                              '0;JMP',	# jump to label END_COMPARE
+                              '(IS_EQUAL_{})'.format(index),	# define label IS_EQUAL
+                              '@SP',
+                              'A=M-1',
+                              'M=-1',	# D=0 so M=True(-1)
+                              '(END_COMPARE_EQ_{})'.format(index)	# define label END_COMPARE
+                              ]
+        elif operator == 'gt':
+            assembly_codes = [
+                              '@SP', 
+                    	      'AM=M-1',	# SP--, A=M-1
+                              'D=M',	# D=Y
+                              'A=A-1',
+                              'D=M-D',	# D=X-Y
+                              '@IS_GREATER_{}'.format(index), 
+                              'D;JGT',	# if D>0 jump to label IS_GREATER
+                              '@SP',
+                              'A=M-1',
+                              'M=0',	# D<=0, M=False
+                              '@END_COMPARE_GT_{}'.format(index),
+                              '0;JMP',	# jump to label END_COMPARE
+                              '(IS_GREATER_{})'.format(index),	# define label IS_GREATER
+                              '@SP',
+                              'A=M-1',
+                              'M=-1',	# D>0 so M=True(-1)
+                              '(END_COMPARE_GT_{})'.format(index)	# define label END_COMPARE
+                              ]
+        elif operator == 'lt':
+            assembly_codes = [
+                              '@SP', 
+                    	      'AM=M-1',	# SP--, A=M-1
+                              'D=M',	# D=Y
+                              'A=A-1',
+                              'D=M-D',	# D=X-Y
+                              '@IS_LESS_{}'.format(index), 
+                              'D;JLT',	# if D<0 jump to label IS_LESS
+                              '@SP',
+                              'A=M-1',
+                              'M=0',	# D>=0, M=False
+                              '@END_COMPARE_LT_{}'.format(index),
+                              '0;JMP',	# jump to label END_COMPARE
+                              '(IS_LESS_{})'.format(index),	# define label IS_LESS
+                              '@SP',
+                              'A=M-1',
+                              'M=-1',	# D<0 so M=True(-1)
+                              '(END_COMPARE_LT_{})'.format(index)	# define label END_COMPARE
+                              ]
+        elif operator == 'and':
+            assembly_codes = [
+                              '@SP', 
+                    	      'AM=M-1',	# SP--, A=M-1
+                              'D=M',	# D=Y
+                              'A=A-1',
+                              'M=D&M',	# M=Y&X
+                              ]
+        elif operator == 'or':
+            assembly_codes = [
+                              '@SP', 
+                    	      'AM=M-1',	# SP--, A=M-1
+                              'D=M',	# D=Y
+                              'A=A-1',
+                              'M=D|M',	# M=Y|X
+                              ]
+        elif operator == 'not':
+            assembly_codes = [
+                              '@SP', 
+                              'A=M-1',
+                              'M=!M',	# Y=!Y
                               ]
         return assembly_codes
 
@@ -37,13 +132,14 @@ class CodeWriter:
     def write(self):
         """Translate and write translated assembly code to out_file"""
         output_codes = []
-        for command_content in self.command_contents:
+        for command_index, command_content in enumerate(self.command_contents):
             cmd_type = command_content[0]
             command = command_content[-1]
             assembly_codes = []
             if cmd_type == 'C_ARITHMETIC':
                 operator = command_content[1]
-                assembly_codes = self.translate_arithmetic(operator)
+                # Pass command_index to translate_arithmetic method for generating unique labels at runtime
+                assembly_codes = self.translate_arithmetic(operator, command_index)
             elif cmd_type == 'C_PUSH' or cmd_type == 'C_POP':
                 memory_segment, index = command_content[1]
                 assembly_codes = self.translate_push_pop(cmd_type, memory_segment, index)
