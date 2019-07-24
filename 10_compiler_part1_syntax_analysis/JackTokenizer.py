00f91649fd0a93d7b6485f1d95e4b3deb1baea2e
broliang <18706703437@163.com>
2019-07-24 21:18:38 +0800

    Add content to JackTokenizer.py: Finish comments handling

--- 10_compiler_part1_syntax_analysis/JackTokenizer.py
+++ 10_compiler_part1_syntax_analysis/JackTokenizer.py
@@ -4,9 +4,47 @@ class JackTokenizer:
         self.in_file = in_file
         self.out_file = out_file
 
+    def tokenize(self):
+        self.process_input()
+        self.write_output()
+
+    def process_input(self):
+        self.skip = False
+        with open(self.in_file, 'r', encoding='utf_8') as inf:
+            for line in inf:
+                if self.should_skip(line):
+                    continue
+                code_content_in_line = self.rm_comments_white_space(line)
+                # Process code_content_in_line
+                tokens_from_line = self.get_tokens_from_code(code_content_in_line)
+                print(code_content_in_line)
+
+    def should_skip(self, line):
+        """Determine whether line parameter should be skiped according different comment type"""
+        # Handle comments in the format /* */
+        if '/*' in line or self.skip:
+            if '*/' in line:
+                self.skip = False
+            else:
+                # if */ not encountered, set skip to True to skip more lines
+                self.skip = True
+            return True
+        # Handle comments in the format // and return code content
+        else:
+            code_content_in_line = self.rm_comments_white_space(line)
+            if not code_content_in_line:
+                # If returned content is none, skip this line
+                return True
+            return False
+        
+    def get_tokens_from_code(self, code_content_in_line):
+        pass
+
+    def rm_comments_white_space(self, line):
+        """Removes all white space and comments start with //"""
+        return line.split('//')[0].strip()
+
     def write_output(self):
         with open(self.out_file, 'w', encoding='utf_8') as outf:
             pass
 
-    def tokenize(self):
-        self.write_output()
