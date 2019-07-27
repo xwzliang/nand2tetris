e502f27b4c17f6065d61f85de28ffe74e4fccb3f
broliang <18706703437@163.com>
2019-07-27 22:04:28 +0800

    Add content to CompilationEngine.py: Start handling subroutineBody

--- 10_compiler_part1_syntax_analysis/CompilationEngine.py
+++ 10_compiler_part1_syntax_analysis/CompilationEngine.py
@@ -90,12 +90,51 @@ class CompilationEngine:
         token, token_type = self.compile_new_token(parent)
         assert token in {'int', 'char', 'boolean'} or token_type == 'identifier'
 
-    def compile_subroutineDec(self, token, token_type):
+    def compile_void_or_type(self, parent):
+        """
+        Compiles type or 'void' for var and add token element to parent.
+        """
+        token, token_type = self.compile_new_token(parent)
+        assert token in {'void', 'int', 'char', 'boolean'} or token_type == 'identifier'
+
+    def compile_subroutineDec(self, token_type, token):
         """
         Compiles a complete method, function, or constructor.
         subroutineDec: ('constructor' | 'function' | 'method') ('void' | type) subroutineName '(' parameterList ')' subroutineBody
         """
-        pass
+        compiled_output_subroutineDec = etree.SubElement(self.compiled_output_root, 'subroutineDec')
+        # Add token in {'constructor', 'function', 'method'} to compiled_output_subroutineDec
+        self.add_sub_element(compiled_output_subroutineDec, token_type, token)
+        self.compile_void_or_type(compiled_output_subroutineDec)
+        # subroutineName
+        self.compile_new_token_ensure_token_type('identifier', compiled_output_subroutineDec)
+        self.compile_new_token_ensure_token('(', compiled_output_subroutineDec)
+        # parameterList
+        compiled_output_parameterList = etree.SubElement(compiled_output_subroutineDec, 'parameterList')
+        token, token_type = self.next_token_and_type()
+        if token == ')':	# No parameter need to add
+            compiled_output_parameterList.text = '\n\t'	# change the print format of empty element compiled_output_parameterList
+            self.add_sub_element(compiled_output_subroutineDec, token_type, token)	# Add ')'
+        else:	# There is at least one parameter needs to be added
+            # type
+            assert token in {'int', 'char', 'boolean'} or token_type == 'identifier'
+            self.add_sub_element(compiled_output_parameterList, token_type, token)	# Add type
+            # varName
+            self.compile_new_token_ensure_token_type('identifier', compiled_output_parameterList)
+            # more paremeters or ')'
+            self.compile_more_parameter_or_right_parenthese(compiled_output_subroutineDec, compiled_output_parameterList)
+        # subroutineBody
+
+    def compile_more_parameter_or_right_parenthese(self, parent_subroutineDec, parent_parameterList):
+        token, token_type = self.next_token_and_type()
+        if token == ')':	# No parameter need to add
+            self.add_sub_element(parent_subroutineDec, token_type, token)	# Add ')'
+        else:
+            assert token == ','
+            self.add_sub_element(parent_parameterList, token_type, token)	# Add ','
+            self.compile_type(parent_parameterList)
+            self.compile_new_token_ensure_token_type('identifier', parent_parameterList)
+            self.compile_more_parameter_or_right_parenthese(parent_subroutineDec, parent_parameterList)
 
     def write_output(self, compiled_etree):
         # output_for_write.write(open(self.out_xml_file, 'w'), encoding='unicode')
