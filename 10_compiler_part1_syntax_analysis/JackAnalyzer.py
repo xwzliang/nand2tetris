7d54d50dcae7b7a07989e6eca04dfcf75dd01039
broliang <18706703437@163.com>
2019-08-09 20:29:30 +0800

    Refactoring for JackAnalyzer.py and JackCompiler.py

--- 10_compiler_part1_syntax_analysis/JackAnalyzer.py
+++ 10_compiler_part1_syntax_analysis/JackAnalyzer.py
@@ -6,14 +6,6 @@ from pathlib import Path
 from JackTokenizer import JackTokenizer 
 from CompilationEngine import CompilationEngine
 
-arg_parser = argparse.ArgumentParser(description='Compile the input Jack program into syntax analyzed output')
-arg_parser.add_argument('path_input', help='The path for Jack program file *.jack or the directory path which contains *.jack files')
-args = arg_parser.parse_args()
-
-
-path_input = Path(args.path_input)
-assert path_input.exists(), "Path does not exist."
-
 def compile_jack(jackfile):
     out_xml_file = jackfile.with_suffix('.xml')
     out_token_xml_file = jackfile.parent / (jackfile.stem + 'T.xml')
@@ -24,13 +16,24 @@ def compile_jack(jackfile):
     jack_compilation_engine = CompilationEngine(tokens_with_tokenType, out_xml_file)
     jack_compilation_engine.compile()
 
-if path_input.is_dir():	# Compile all jack files in directory
-    jackfiles = [f for f in path_input.glob('*.jack')]
-    assert jackfiles, "No jack file in this directory."
-    for jackfile in jackfiles:
-        compile_jack(jackfile)
-else:	# Compile one jack file
-    jackfile = path_input
-    compile_jack(jackfile)
+def main():
+    arg_parser = argparse.ArgumentParser(description='Compile the input Jack program into syntax analyzed output')
+    arg_parser.add_argument('path_input', help='The path for Jack program file *.jack or the directory path which contains *.jack files')
+    args = arg_parser.parse_args()
 
-print('Done')
+    path_input = Path(args.path_input)
+    assert path_input.exists(), "Path does not exist."
+
+    if path_input.is_dir():	# Compile all jack files in directory
+        jackfiles = [f for f in path_input.glob('*.jack')]
+        assert jackfiles, "No jack file in this directory."
+        for jackfile in jackfiles:
+            compile_jack(jackfile)
+    else:	# Compile one jack file
+        jackfile = path_input
+        compile_jack(jackfile)
+
+    print('Done')
+
+if __name__ == '__main__':
+    main()
